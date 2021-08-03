36000ff45e7693a5d78a051c299f113c229261f9
Altho <alan.at.thomas@gmail.com>
2021-08-04 00:58:27 +0200

    all logic finished

--- src/elements.js
+++ src/elements.js
@@ -1,20 +1,33 @@
 import restaurant from './restaurant.jpeg';
+import tooth from './tooth.svg';
 
 function importAll(r) {
     let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    r.keys().map((item, index) => {
+        images[item.replace('./', '')] = r(item);
+    });
+    console.log(images);
     return images;
 }
 
+
 const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
-console.log(images);
 
 const genFunctions = (() => {
+    const navMenuChoice = (choice) => {
+        console.log('ok');
+
+    };
     const welcome = (title, image, content) => {
+        // const container = document.querySelector('#container');
+        // document.body.removeChild(container);
+        // const containerCreate = document.createElement('div');
+        // containerCreate.id = "container";
+        // document.body.appendChild(containerCreate);
+
         const titleRestaurant = document.createElement('H1');
         titleRestaurant.innerHTML = title;
-        const container = document.querySelector('#container');
         container.appendChild(titleRestaurant)
         const imgDiv = document.createElement('div');
         imgDiv.classList.add('img-div');
@@ -43,21 +56,27 @@ const genFunctions = (() => {
 
         return header;
     };
+    function removeContainer() {
+        const container = document.querySelector('#container');
+        document.body.removeChild(container);
+    };
 
     function navBarGen(...items) {
         console.log(items);
         const navBar = document.createElement('nav');
         navBar.id = "navbar";
         let menuList = document.createElement('ul');
-
+        let concat = 1;
         for (let i = 0; i < items.length; i++) {
             const listElement = document.createElement('li');
-            console.log(items[i]);
+            listElement.classList.add('item');
+            listElement.classList.add('menu-item'+concat);
+            concat++;
             listElement.innerHTML = items[i];
             menuList.appendChild(listElement);
-            menuList.addEventListener('click', genFunctions.menuGen);
 
         }
+        concat = 1;
         navBar.appendChild(menuList);
         return navBar;
 
@@ -68,31 +87,68 @@ const genFunctions = (() => {
         container.id = "container";
         return container;
 
+
+
     };
 
-    function menuGen(menu){
+    function menuGen(menu) {
         const container = document.querySelector('#container');
-        for (let item of menu){
+        const menuFlex = document.createElement('div');
+        menuFlex.id='menu-flex';
+        container.appendChild(menuFlex);
+        let imgValue;
+        for (let i = 0; i < menu.length; i++) {
+
             const card = document.createElement('div');
-            card.classList.add('.card');
+            card.classList.add('card');
             const cardtitle = document.createElement('div');
             cardtitle.classList.add('card-title')
-            cardtitle.innerHTML=item.title;
+            cardtitle.innerHTML = menu[i].title;
+            card.appendChild(cardtitle);
             const cardImg = document.createElement('img');
-            cardImg.src= images[‘item.img’];
-            console.log(cardImg.src);
+            const test = Object.keys(images)[i];
+            console.log(menu[i].img)
+            const imgDiv = document.createElement('div');
+            imgDiv.classList.add('img-div');
+
+            for (const property in images) {
+                if (property == menu[i].img) {
+
+
+                    cardImg.src = (images[property]);
+                    imgDiv.appendChild(cardImg);
+                    card.appendChild(imgDiv);
+
+
+                }
+
+
+            }
+
+
+            const cardPrice = document.createElement('div');
+            const toothIcon = new Image();
+            toothIcon.src = tooth;
+            cardPrice.classList.add('card-price');
+            cardPrice.innerHTML=menu[i].price;
+            cardPrice.appendChild(toothIcon);
+            card.appendChild(cardPrice);
+            const cardContent = document.createElement('div');
+            cardContent.classList.add('card-content');
+            cardContent.innerHTML=menu[i].content;
+            card.appendChild(cardContent);
+            menuFlex.appendChild(card);
+
+
         }
+
     }
 
-    function removeContainer() {
-        const container = document.querySelector('#container');
-        document.body.removeChild(container);
-    }
 
-    return {welcome, containerGen,removeContainer,navBarGen,headerGen, menuGen};
+
+    return {welcome, containerGen, removeContainer, navBarGen, headerGen, menuGen, navMenuChoice};
 
 })();
-
 
 
 export {
