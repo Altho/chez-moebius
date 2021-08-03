251d861015d2b5f3741b4c86f5892e640688849f
Altho <alan.at.thomas@gmail.com>
2021-08-03 11:41:21 +0200

    Reorganized logic

--- src/elements.js
+++ src/elements.js
@@ -1,55 +1,78 @@
+import restaurant from './restaurant.jpeg';
 
 
 
-function  headerGen() {
+const genFunctions = (() => {
+    const welcome = (title, image, content) => {
+        const titleRestaurant = document.createElement('H1');
+        titleRestaurant.innerHTML = title;
+        const container = document.querySelector('#container');
+        container.appendChild(titleRestaurant)
+        const imgDiv = document.createElement('div');
+        imgDiv.classList.add('img-div');
+        const RestaurantImg = new Image();
+        RestaurantImg.src = restaurant;
+        imgDiv.appendChild(RestaurantImg);
+        container.appendChild(imgDiv);
+        const contentRestaurant = document.createElement('div');
+        contentRestaurant.classList.add('content');
+        contentRestaurant.innerHTML = content;
+        container.appendChild(contentRestaurant);
+        const signature = document.createElement('p');
+        signature.id = "signature";
+        signature.innerHTML = "Come on down, chez Moebius !"
+        container.appendChild(signature);
+    };
 
-const header = document.createElement('header');
-header.id="header";
-header.innerHTML ="This is the header"
+    function headerGen(title) {
+        const header = document.createElement('header');
+        header.id = "header";
 
-return header;
-};
+        const headerTitle = document.createElement('h1');
+        headerTitle.innerHTML = title;
+        header.appendChild(headerTitle);
 
-function navBarGen(...items)  {
-    console.log(items);
-    const navBar = document.createElement('nav');
-    navBar.id="navbar";
-    let menuList = document.createElement('ul');
 
-    for(let item of items){
-        const listElement = document.createElement('li');
-        console.log(item);
-        listElement.innerHTML=item;
-        menuList.appendChild(listElement);
+        return header;
+    };
 
+    function navBarGen(...items) {
+        console.log(items);
+        const navBar = document.createElement('nav');
+        navBar.id = "navbar";
+        let menuList = document.createElement('ul');
+
+        for (let i = 0; i < items.length; i++) {
+            const listElement = document.createElement('li');
+            console.log(items[i]);
+            listElement.innerHTML = items[i];
+            menuList.appendChild(listElement);
+            menuList.addEventListener('click', genFunctions.removeContainer);
+
+        }
+        navBar.appendChild(menuList);
+        return navBar;
+
+    };
+
+    function containerGen() {
+        const container = document.createElement('div');
+        container.id = "container";
+        return container;
+
+    };
+
+    function removeContainer() {
+        const container = document.querySelector('#container');
+        document.body.removeChild(container);
     }
-    navBar.appendChild(menuList);
-    return navBar;
 
-}
+    return {welcome, containerGen,removeContainer,navBarGen,headerGen};
 
-function containerGen(){
-    const container = document.createElement('div');
-    container.id="container";
-    return container;
+})();
 
-}
 
-function restaurantPageGen(title,image,content){
-    const titleRestaurant = document.createElement('H1');
-    titleRestaurant.innerHTML=title;
-    const container = document.querySelector('#container');
-    container.appendChild(titleRestaurant)
-    const imgRestaurant = document.createElement('img');
-    imgRestaurant.src=image;
-    container.appendChild(imgRestaurant);
-    const contentRestaurant = document.createElement('div');
-    contentRestaurant.id="content-restaurant";
-    contentRestaurant.innerHTML=content;
-    container.appendChild(contentRestaurant);
-    return titleRestaurant, contentRestaurant;
-}
 
 export {
-    headerGen,navBarGen,containerGen,restaurantPageGen
+    genFunctions
 };
--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import './style.css';
-import {headerGen, navBarGen,containerGen,restaurantPageGen} from  './elements.js';
+import {genFunctions} from  './elements.js';
 
 const welcomeContent = {
-    title : "Welcome Chez Moëbius !",
+    title : "Welcome chez Moebius !",
     img : "./restaurant.jpeg",
-    content : "Ideally located at the multiverse cornerstone, experience fine dining in a restaurant overlooking not more than 32 dimensions ! We provide sustenance to most carbon-based lives, extra-dimensional beings and ethereal entities.Please note that Chez Moëbius has 5 time dimensions, time traveling is not advised and we decline all responsibilty for any potential injury (becoming the void, being turned inside out, experiencing dimension 0 or absorbing every conscioussness and becoming a mad god. "
+    content : "Ideally located at the multiverse cornerstone, experience fine dining in a restaurant overlooking not less than 32 dimensions ! We provide sustenance to most carbon-based lives, extra-dimensional beings and ethereal entities.Please note that Chez Moëbius is built on top 5 time dimensions, time traveling is not advised. We decline all responsibilty for any potential injury (becoming the void, being turned inside out, experiencing dimension 0 or absorbing every conscioussness and becoming a mad god. For parties starting at ∞+1 please make sure to call us ahead so me can make the required arrangements. Please note that we only accept STC (Standard tooth currency)"
 
 
 }
 
 
-document.body.appendChild(headerGen());
-document.body.appendChild(navBarGen("Our restaurant", "Menu", "Contact us"));
-document.body.appendChild(containerGen());
-restaurantPageGen(welcomeContent.title,welcomeContent.img,welcomeContent.content);
+document.body.appendChild(genFunctions.headerGen("Chez Moebius"));
+document.body.appendChild(genFunctions.navBarGen("Our restaurant", "Menu", "Contact us"));
+document.body.appendChild(genFunctions.containerGen());
+document.body.appendChild(genFunctions.welcome(welcomeContent.title,welcomeContent.img,welcomeContent.content));
+
 
